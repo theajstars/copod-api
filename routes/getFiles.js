3e3d1b0815481a923fa48034dfc4cf9ef39abe69
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2022-05-10 23:20:27 +0100

    Added Top Half of new Israeli dataset

--- routes/getFiles.js
+++ routes/getFiles.js
@@ -6,6 +6,10 @@ const testBottom575 = require("../assets/COPOD_Sets/testBottom575.json");
 const trainBottom2000 = require("../assets/COPOD_Sets/trainBottom2000.json");
 const testTop575 = require("../assets/COPOD_Sets/testTop575.json");
 const trainTop2000 = require("../assets/COPOD_Sets/trainTop2000.json");
+
+const newDataSets = {
+  topHalf: require("../assets/COPOD_Sets/Israeli_Datasets/df500000.json"),
+};
 app.get("/datasets/bottomhalf", (req, res) => {
   res.json({
     file: bottomHalf,
@@ -34,5 +38,10 @@ app.get("/datasets/top575", (req, res) => {
 app.get("/datasets/top2000", (req, res) => {
   res.json({
     file: trainTop2000,
+  });
+});
+app.get("/datasets/new/tophalf", (req, res) => {
+  res.json({
+    file: newDataSets.topHalf,
   });
 });
